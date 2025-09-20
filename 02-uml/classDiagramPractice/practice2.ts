c87f1eb719dfb8191eaa018d0fc7ad2748b78029
exag <hukuti145@gmail.com>
2025-09-20 20:32:57 +0900

    // クラス図 演習2

--- 02-uml/classDiagramPractice/practice2.ts
+++ 02-uml/classDiagramPractice/practice2.ts
@@ -1,3 +1,26 @@
 export {};
 
 // クラス図 演習2
+interface Shape {
+  calcArea(): number;
+}
+
+class Client {
+  private shape: Shape;
+}
+
+class Rectangle {
+  private width: number;
+  private height: number;
+
+  calcArea(): number {
+    return this.width * this.height;
+  }
+}
+class Circle {
+  private radius: number;
+
+  calcArea(): number {
+    return this.radius * this.radius * Math.PI;
+  }
+}
