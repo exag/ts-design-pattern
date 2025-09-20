1771be91bae772d33935b64df5248047e0887f5d
exag <hukuti145@gmail.com>
2025-09-20 21:18:46 +0900

    16. オープンクローズドの原則【コーディング Part1】

--- 03-solid-principle/openClosed/badExample.ts
+++ 03-solid-principle/openClosed/badExample.ts
@@ -1,7 +1,7 @@
 export {};
 
 // 従業員の職位
-type Grade = 'junior' | 'middle' | 'senior';
+type Grade = "junior" | "middle" | "senior" | "expert";
 
 class Employee {
   constructor(public name: string, public grade: Grade) {}
@@ -11,12 +11,14 @@ class BonusCalculator {
   constructor(public base: number) {}
 
   getBonus(employee: Employee): number {
-    if (employee.grade === 'junior') {
+    if (employee.grade === "junior") {
       return Math.floor(this.base * 1.1);
-    } else if (employee.grade === 'middle') {
+    } else if (employee.grade === "middle") {
       return Math.floor(this.base * 1.5);
-    } else if (employee.grade === 'senior') {
+    } else if (employee.grade === "senior") {
       return Math.floor(this.base * 2);
+    } else if (employee.grade === "expert") {
+      return Math.floor(this.base * 2.5);
     } else {
       return Math.floor(this.base * 3);
     }
@@ -24,15 +26,17 @@ class BonusCalculator {
 }
 
 function run() {
-  const emp1 = new Employee('Yamada', 'junior');
-  const emp2 = new Employee('Suzuki', 'middle');
-  const emp3 = new Employee('Tanaka', 'senior');
+  const emp1 = new Employee("Yamada", "junior");
+  const emp2 = new Employee("Suzuki", "middle");
+  const emp3 = new Employee("Tanaka", "senior");
+  const emp4 = new Employee("Saitou", "expert");
 
   const bonusCalculator = new BonusCalculator(100);
 
   console.log(bonusCalculator.getBonus(emp1));
   console.log(bonusCalculator.getBonus(emp2));
   console.log(bonusCalculator.getBonus(emp3));
+  console.log(bonusCalculator.getBonus(emp4));
 }
 
 run();
